4d846ce4e264be11a520929baddfade5dd36694f
Thai-VuDuc169 <vuducthaibka62@gmail.com>
2021-10-08 21:29:17 +0700

    adding code to detections

---
+++ test/infer_to_features/build/CMakeFiles/test_infer2features.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_infer2features.dir/main.cpp.o"
+  "test_infer2features.pdb"
+  "test_infer2features"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_infer2features.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
