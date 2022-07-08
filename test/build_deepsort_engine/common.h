a90dc0b2b28ae23a4946dee3a550fc27a8f32a02
Thai-VuDuc169 <vuducthaibka62@gmail.com>
2022-07-08 18:00:58 +0700

    [build_deepsort_engine] add cpp code to gen ds engine using trt api

--- test/build_deepsort_engine/common.h
+++ test/build_deepsort_engine/common.h
@@ -12,64 +12,97 @@
 
 using namespace nvinfer1;
 
-IScaleLayer* addBatchNorm2d(INetworkDefinition *network, std::map<std::string, Weights>& weightMap, ITensor& input, std::string lname, float eps) 
+IScaleLayer* addBatchNorm2d(INetworkDefinition* network, std::map<std::string, Weights>& weightMap, ITensor& input, std::string lname, float eps)
 {
-    float *gamma = (float*)weightMap[lname + ".weight"].values;
-    float *beta = (float*)weightMap[lname + ".bias"].values;
-    float *mean = (float*)weightMap[lname + ".running_mean"].values;
-    float *var = (float*)weightMap[lname + ".running_var"].values;
-    int len = weightMap[lname + ".running_var"].count;
+    // float* gamma = (float*)weightMap[lname + "gamma"].values; // scale
+    float gamma = 1.0; // const scale
+    float* beta = (float*)weightMap[lname + "/beta"].values;   // offset
+    float* mean = (float*)weightMap[lname + "/moving_mean"].values;
+    float* var = (float*)weightMap[lname + "/moving_variance"].values;
+    int len = weightMap[lname + "/moving_variance"].count;
 
-    float *scval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
-    for (int i = 0; i < len; i++) {
-        scval[i] = gamma[i] / sqrt(var[i] + eps);
+    float* scval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
+    for (auto i = 0; i < len; i++)
+    {
+        scval[i] = gamma / sqrt(var[i] + eps);
     }
     Weights scale{ DataType::kFLOAT, scval, len };
 
-    float *shval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
-    for (int i = 0; i < len; i++) {
-        shval[i] = beta[i] - mean[i] * gamma[i] / sqrt(var[i] + eps);
+    float* shval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
+    for (auto i = 0; i < len; i++)
+    {
+        shval[i] = beta[i] - mean[i] * gamma / sqrt(var[i] + eps);
     }
     Weights shift{ DataType::kFLOAT, shval, len };
 
-    float *pval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
-    for (int i = 0; i < len; i++) {
+    float* pval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
+    for (auto i = 0; i < len; i++)
+    {
         pval[i] = 1.0;
     }
     Weights power{ DataType::kFLOAT, pval, len };
 
-    weightMap[lname + ".scale"] = scale;
-    weightMap[lname + ".shift"] = shift;
-    weightMap[lname + ".power"] = power;
+    weightMap[lname + "/scale"] = scale;
+    weightMap[lname + "/shift"] = shift;
+    weightMap[lname + "/power"] = power;
     IScaleLayer* scale_1 = network->addScale(input, ScaleMode::kCHANNEL, shift, scale, power);
     assert(scale_1);
     return scale_1;
 }
 
-ILayer* convBlock(INetworkDefinition *network, std::map<std::string, Weights>& weightMap, ITensor& input, int outch, int ksize, int s, int g, std::string lname) 
+ILayer* innerBlock(INetworkDefinition *network, std::map<std::string, Weights>& weightMap, ITensor& input, int outch, std::string lname, bool increase_dim= true)
 {
     Weights emptywts{ DataType::kFLOAT, nullptr, 0 };
-    int p = ksize / 3;
-    IConvolutionLayer* conv1 = network->addConvolutionNd(input, outch, DimsHW{ ksize, ksize }, weightMap[lname + ".conv.weight"], emptywts);
+    int out_features = outch;
+    int stride = 1;
+    if (increase_dim)
+        stride = 2;
+    IConvolutionLayer* conv1 = network->addConvolutionNd(input, out_features, DimsHW{3, 3}, weightMap[lname + "/1/weights"], emptywts);
+    conv1->setPaddingNd(DimsHW{ 1, 1 });
+    conv1->setStrideNd(DimsHW{stride, stride});
     assert(conv1);
-    conv1->setStrideNd(DimsHW{ s, s });
-    conv1->setPaddingNd(DimsHW{ p, p });
-    conv1->setNbGroups(g);
-    IScaleLayer* bn1 = addBatchNorm2d(network, weightMap, *conv1->getOutput(0), lname + ".bn", 1e-3);
+    IScaleLayer* conv1_bn = addBatchNorm2d(network, weightMap, *conv1->getOutput(0), lname + "/1/" + lname + "/1/bn" , 1e-3); // esp = 1e-5 ? thay doi tuy theo dau ra mong muon
+    assert(conv1_bn);
+    IActivationLayer* conv1_elu = network->addActivation(*conv1_bn->getOutput(0), ActivationType::kELU);
+    assert(conv1_elu);
 
-    // silu = x * sigmoid
-    auto sig = network->addActivation(*bn1->getOutput(0), ActivationType::kSIGMOID);
-    assert(sig);
-    auto ew = network->addElementWise(*bn1->getOutput(0), *sig->getOutput(0), ElementWiseOperation::kPROD);
-    assert(ew);
-    return ew;
+    IConvolutionLayer* conv2 = network->addConvolutionNd(*conv1_elu->getOutput(0), out_features, DimsHW{3, 3}, weightMap[lname + "/2/weights"], weightMap[lname + "/2/biases"]);
+    conv2->setPaddingNd(DimsHW{ 1, 1 });
+    assert(conv2);
+    return conv2;
 }
 
-ILayer* residualBlock(INetworkDefinition *network, std::map<std::string, Weights>& weightMap, ITensor& input, int outch, int ksize, int s, int g, std::string lname) 
+ILayer* residualBlock(INetworkDefinition *network, std::map<std::string, Weights>& weightMap, ITensor& input, int outch, std::string lname, bool increase_dim = false, bool is_first = false) 
 {
+    Weights emptywts{ DataType::kFLOAT, nullptr, 0 };
+    ITensor *pre_block_network = &input;
+    if (!is_first)
+    //     pre_block_network = input;
+    // else
+    {
+        IScaleLayer* conv1_pre_bn = addBatchNorm2d(network, weightMap, input, lname + "/bn" , 1e-3); // esp = 1e-5 ? thay doi tuy theo dau ra mong muon
+        assert(conv1_pre_bn);
+        IActivationLayer* conv1_pre_elu = network->addActivation(*conv1_pre_bn->getOutput(0), ActivationType::kELU);
+        assert(conv1_pre_elu);
+        pre_block_network = conv1_pre_elu->getOutput(0);
+    }
     
+    auto innner_block = innerBlock(network, weightMap, *pre_block_network, outch, lname, increase_dim);
+    IElementWiseLayer* element_wise;
+    if (increase_dim)
+    {
+        auto projection_block = network->addConvolutionNd(input, outch, DimsHW{1, 1}, weightMap[lname + "/projection/weights"], emptywts);
+        projection_block->setStrideNd(DimsHW{2, 2});
+        assert(projection_block);
+        element_wise = network->addElementWise(*innner_block->getOutput(0), *projection_block->getOutput(0), ElementWiseOperation::kSUM);
+        assert(element_wise);
+    }
+    else
+    {
+        element_wise = network->addElementWise(*innner_block->getOutput(0), input, ElementWiseOperation::kSUM);
+        assert(element_wise);
+    }
+    return element_wise;
 }
-
-
 
 #endif
--- test/build_deepsort_engine/mars.cpp
+++ test/build_deepsort_engine/mars.cpp
@@ -1,9 +1,11 @@
 #include "NvInfer.h"
 #include "cuda_runtime_api.h"
+#include "common.h"
 #include "logging.h"
 #include <fstream>
 #include <map>
 #include <chrono>
+#include <cassert>
 
 #define CHECK(status) \
     do\
@@ -21,7 +23,7 @@ static const int INPUT_H = 128;
 static const int INPUT_W = 64;
 static const int OUTPUT_SIZE = 128;
 
-#define WEIGHT_MAP_PATH ""
+#define WEIGHT_MAP_PATH "/home/thaivu169/Projects/Turnstiles_Fare_Evasion/test/gen_wts_deepsort/mars-small128.wts"
 
 const char* INPUT_BLOB_NAME = "data";
 const char* OUTPUT_BLOB_NAME = "prob";
@@ -77,72 +79,64 @@ std::map<std::string, Weights> loadWeights(const std::string file)
 ICudaEngine* createMarsEngine(unsigned int maxBatchSize, IBuilder* builder, IBuilderConfig* config, DataType dt)
 {
     INetworkDefinition* network = builder->createNetworkV2(0U);
-
+    Weights emptywts{ DataType::kFLOAT, nullptr, 0 };
     // Create input tensor of shape { 1, 128, 64, 3} with name INPUT_BLOB_NAME
-    ITensor* data = network->addInput(INPUT_BLOB_NAME, dt, Dims3{1, INPUT_H, INPUT_W});
+    ITensor* data = network->addInput(INPUT_BLOB_NAME, dt, Dims3{3, INPUT_H, INPUT_W});
     assert(data);
 
     std::map<std::string, Weights> weightMap = loadWeights(WEIGHT_MAP_PATH);
 
-    // continue!!!
-    // Add convolution layer with 6 outputs and a 5x5 filter.
-    IConvolutionLayer* conv1 = network->addConvolutionNd(*data, 6, DimsHW{5, 5}, weightMap["conv1.weight"], weightMap["conv1.bias"]);
-    assert(conv1);
-    conv1->setStrideNd(DimsHW{1, 1});
-
-    // Add activation layer using the ReLU algorithm.
-    IActivationLayer* relu1 = network->addActivation(*conv1->getOutput(0), ActivationType::kRELU);
-    assert(relu1);
-
-    // Add max pooling layer with stride of 2x2 and kernel size of 2x2.
-    IPoolingLayer* pool1 = network->addPoolingNd(*relu1->getOutput(0), PoolingType::kAVERAGE, DimsHW{2, 2});
+    // ================================== Define an empty network using TensorRT API ==================================
+    // =====>>> conv1_* : conv1_1
+    IConvolutionLayer* conv1_1 = network->addConvolutionNd(*data, 32, DimsHW{ 3, 3 }, weightMap["conv1_1/weights"], emptywts);
+    conv1_1->setPaddingNd(DimsHW{ 1, 1 });
+    assert(conv1_1);
+    IScaleLayer* conv1_1_bn = addBatchNorm2d(network, weightMap, *conv1_1->getOutput(0), "conv1_1/conv1_1/bn", 1e-3); // esp = 1e-5 ? thay doi tuy theo sai so dau ra mong muon
+    assert(conv1_1_bn);
+    IActivationLayer* conv1_1_elu = network->addActivation(*conv1_1_bn->getOutput(0), ActivationType::kELU);
+    assert(conv1_1_elu);
+    // =====>>> conv1_* : conv1_2
+    IConvolutionLayer* conv1_2 = network->addConvolutionNd(*conv1_1_elu->getOutput(0), 32, DimsHW{ 3, 3 }, weightMap["conv1_2/weights"], emptywts);
+    conv1_2->setPaddingNd(DimsHW{ 1, 1 });
+    assert(conv1_2);
+    IScaleLayer* conv1_2_bn = addBatchNorm2d(network, weightMap, *conv1_2->getOutput(0), "conv1_2/conv1_2/bn", 1e-3); // esp = 1e-5 ? thay doi tuy theo sai so dau ra mong muon
+    assert(conv1_2_bn);
+    IActivationLayer* conv1_2_elu = network->addActivation(*conv1_2_bn->getOutput(0), ActivationType::kELU);
+    assert(conv1_2_elu);
+    // add MaxPool layer
+    IPoolingLayer* pool1 = network->addPoolingNd(*conv1_2_elu->getOutput(0), PoolingType::kMAX, DimsHW{3, 3});
     assert(pool1);
+    pool1->setPaddingNd(DimsHW{1, 1});
     pool1->setStrideNd(DimsHW{2, 2});
 
-    // Add second convolution layer with 16 outputs and a 5x5 filter.
-    IConvolutionLayer* conv2 = network->addConvolutionNd(*pool1->getOutput(0), 16, DimsHW{5, 5}, weightMap["conv2.weight"], weightMap["conv2.bias"]);
-    assert(conv2);
-    conv2->setStrideNd(DimsHW{1, 1});
-
-    // Add activation layer using the ReLU algorithm.
-    IActivationLayer* relu2 = network->addActivation(*conv2->getOutput(0), ActivationType::kRELU);
-    assert(relu2);
-
-    // Add second max pooling layer with stride of 2x2 and kernel size of 2x2>
-    IPoolingLayer* pool2 = network->addPoolingNd(*relu2->getOutput(0), PoolingType::kAVERAGE, DimsHW{2, 2});
-    assert(pool2);
-    pool2->setStrideNd(DimsHW{2, 2});
-
-    // Add fully connected layer
-    IFullyConnectedLayer* fc1 = network->addFullyConnected(*pool2->getOutput(0), 120, weightMap["fc1.weight"], weightMap["fc1.bias"]);
+    // =====>>> conv2_*
+    auto conv2_1 = residualBlock(network, weightMap, *pool1->getOutput(0), 32, "conv2_1", false, true);
+    auto conv2_3 = residualBlock(network, weightMap, *conv2_1->getOutput(0), 32, "conv2_3", false, false);
+    // =====>>> conv3_*
+    auto conv3_1 = residualBlock(network, weightMap, *conv2_3->getOutput(0), 64, "conv3_1", true, false);
+    auto conv3_3 = residualBlock(network, weightMap, *conv3_1->getOutput(0), 64, "conv3_3", false, false);
+    // =====>>> conv4_*
+    auto conv4_1 = residualBlock(network, weightMap, *conv3_3->getOutput(0), 128, "conv4_1", true, false);
+    auto conv4_3 = residualBlock(network, weightMap, *conv4_1->getOutput(0), 128, "conv4_3", false, false);
+    // =====>>> fully connected layer
+    IFullyConnectedLayer* fc1 = network->addFullyConnected(*conv4_3->getOutput(0), 128, weightMap["fc1/weights"], emptywts);
     assert(fc1);
+    IScaleLayer* fc1_bn = addBatchNorm2d(network, weightMap, *fc1->getOutput(0), "fc1/fc1/bn", 1e-3); // esp = 1e-5 ? thay doi tuy theo sai so dau ra mong muon
+    assert(fc1_bn);
+    IActivationLayer* fc1_elu = network->addActivation(*fc1_bn->getOutput(0), ActivationType::kELU);
+    assert(fc1_elu);
+    
+    auto prob = fc1_elu;
 
-    // Add activation layer using the ReLU algorithm.
-    IActivationLayer* relu3 = network->addActivation(*fc1->getOutput(0), ActivationType::kRELU);
-    assert(relu3);
-
-    // Add second fully connected layer
-    IFullyConnectedLayer* fc2 = network->addFullyConnected(*relu3->getOutput(0), 84, weightMap["fc2.weight"], weightMap["fc2.bias"]);
-    assert(fc2);
-
-    // Add activation layer using the ReLU algorithm.
-    IActivationLayer* relu4 = network->addActivation(*fc2->getOutput(0), ActivationType::kRELU);
-    assert(relu4);
-
-    // Add third fully connected layer
-    IFullyConnectedLayer* fc3 = network->addFullyConnected(*relu4->getOutput(0), OUTPUT_SIZE, weightMap["fc3.weight"], weightMap["fc3.bias"]);
-    assert(fc3);
-
-    // Add softmax layer to determine the probability.
-    ISoftMaxLayer* prob = network->addSoftMax(*fc3->getOutput(0));
-    assert(prob);
     prob->getOutput(0)->setName(OUTPUT_BLOB_NAME);
     network->markOutput(*prob->getOutput(0));
 
     // Build engine
     builder->setMaxBatchSize(maxBatchSize);
     config->setMaxWorkspaceSize(1 << 20);
+    std::cout << "Building engine, please wait for a while..." << std::endl;
     ICudaEngine* engine = builder->buildEngineWithConfig(*network, *config);
+    std::cout << "Build engine successfully!" << std::endl;
 
     // Don't need the network any more
     network->destroy();
@@ -189,7 +183,7 @@ void doInference(IExecutionContext& context, float* input, float* output, int ba
     const int outputIndex = engine.getBindingIndex(OUTPUT_BLOB_NAME);
 
     // Create GPU buffers on device
-    CHECK(cudaMalloc(&buffers[inputIndex], batchSize * INPUT_H * INPUT_W * sizeof(float)));
+    CHECK(cudaMalloc(&buffers[inputIndex], batchSize * 3 * INPUT_H * INPUT_W * sizeof(float)));
     CHECK(cudaMalloc(&buffers[outputIndex], batchSize * OUTPUT_SIZE * sizeof(float)));
 
     // Create stream
@@ -197,7 +191,7 @@ void doInference(IExecutionContext& context, float* input, float* output, int ba
     CHECK(cudaStreamCreate(&stream));
 
     // DMA input batch data to device, infer on the batch asynchronously, and DMA output back to host
-    CHECK(cudaMemcpyAsync(buffers[inputIndex], input, batchSize * INPUT_H * INPUT_W * sizeof(float), cudaMemcpyHostToDevice, stream));
+    CHECK(cudaMemcpyAsync(buffers[inputIndex], input, batchSize * 3 * INPUT_H * INPUT_W * sizeof(float), cudaMemcpyHostToDevice, stream));
     context.enqueue(batchSize, buffers, stream, nullptr);
     CHECK(cudaMemcpyAsync(output, buffers[outputIndex], batchSize * OUTPUT_SIZE * sizeof(float), cudaMemcpyDeviceToHost, stream));
     cudaStreamSynchronize(stream);
@@ -212,8 +206,8 @@ int main(int argc, char** argv)
 {
     if (argc != 2) {
         std::cerr << "arguments not right!" << std::endl;
-        std::cerr << "./lenet -s   // serialize model to plan file" << std::endl;
-        std::cerr << "./lenet -d   // deserialize plan file and run inference" << std::endl;
+        std::cerr << "./mars -s   // serialize model to plan file" << std::endl;
+        std::cerr << "./mars -d   // deserialize plan file and run inference" << std::endl;
         return -1;
     }
 
@@ -226,7 +220,7 @@ int main(int argc, char** argv)
         APIToModel(1, &modelStream);
         assert(modelStream != nullptr);
 
-        std::ofstream p("lenet5.engine", std::ios::binary);
+        std::ofstream p("mars.engine", std::ios::binary);
         if (!p)
         {
             std::cerr << "could not open plan output file" << std::endl;
@@ -236,7 +230,7 @@ int main(int argc, char** argv)
         modelStream->destroy();
         return 1;
     } else if (std::string(argv[1]) == "-d") {
-        std::ifstream file("lenet5.engine", std::ios::binary);
+        std::ifstream file("mars.engine", std::ios::binary);
         if (file.good()) {
             file.seekg(0, file.end);
             size = file.tellg();
@@ -252,16 +246,17 @@ int main(int argc, char** argv)
 
 
     // Subtract mean from image
-    float data[INPUT_H * INPUT_W];
-    for (int i = 0; i < INPUT_H * INPUT_W; i++)
-        data[i] = 1.0;
+    static float data[3 * INPUT_H * INPUT_W];
+    for (int i = 0; i < 3 * INPUT_H * INPUT_W; i++)
+        data[i] = 1;
 
     IRuntime* runtime = createInferRuntime(gLogger);
     assert(runtime != nullptr);
-    ICudaEngine* engine = runtime->deserializeCudaEngine(trtModelStream, size, nullptr);
+    ICudaEngine* engine = runtime->deserializeCudaEngine(trtModelStream, size);
     assert(engine != nullptr);
     IExecutionContext* context = engine->createExecutionContext();
     assert(context != nullptr);
+    delete[] trtModelStream;
 
     // Run inference
     float prob[OUTPUT_SIZE];
@@ -269,7 +264,7 @@ int main(int argc, char** argv)
         auto start = std::chrono::system_clock::now();
         doInference(*context, data, prob, 1);
         auto end = std::chrono::system_clock::now();
-        //std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
     }
 
     // Destroy the engine
@@ -279,7 +274,7 @@ int main(int argc, char** argv)
 
     // Print histogram of the output distribution
     std::cout << "\nOutput:\n\n";
-    for (unsigned int i = 0; i < 10; i++)
+    for (unsigned int i = 0; i < OUTPUT_SIZE; i++)
     {
         std::cout << prob[i] << ", ";
     }
@@ -287,33 +282,3 @@ int main(int argc, char** argv)
 
     return 0;
 }
-
-
-
-// // ========== TEST CODE ========== 
-// #include <opencv2/core.hpp>
-// #include <iostream>
-// using namespace std;
-// using namespace cv;
-// int main()
-
-// {
-//     uchar d[3][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-
-//     // Mat m(2, 2, CV_8UC3);
-//     Mat_<uchar> m[3];
-//     Mat_<uchar> a =  (Mat_<uchar>(2,2) << 1 , 2 , 3, 4);
-//     Mat_<uchar> b = (Mat_<uchar>(2,2) << 5, 6, 7, 8);
-//     Mat_<uchar> c = (Mat_<uchar>(2,2) << 9, 10, 11, 12);
-//     m[0] = a;
-//     m[1] = b;
-//     m[2] = c;
-//     Mat zx; 
-//     cv::merge(m, 3, zx);
-//     // m<< 1 << 2 << 3<<4<<5<<6<<7<<8<<9<<10<<11<<12;
-//     Mat channels[3];
-//     cout << "zx: " << zx << endl;
-//     cout << "a: " << a << endl;
-//     cout << "b: " << b << endl;
-//     return 0;
-// }
